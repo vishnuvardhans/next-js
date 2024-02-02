067ce2a7a8c14109e98d1727774227d1ca6c0257
dinesh <dineshkumar.p@unilever.com>
2024-02-02 13:09:28 +0530

    till chapter  9 half skelition error solution

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -8,22 +8,22 @@ import {
   User,
   Revenue,
 } from './definitions';
+import { unstable_noStore as noStore } from 'next/cache';
 import { formatCurrency } from './utils';
-
 export async function fetchRevenue() {
-  // Add noStore() here to prevent the response from being cached.
+  noStore();
   // This is equivalent to in fetch(..., {cache: 'no-store'}).
 
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
+    console.log('Data fetch completed after 3 seconds.');
 
     return data.rows;
   } catch (error) {
